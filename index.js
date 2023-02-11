47171d5131df84337a81a53b0f1bdf60b80902a6
Zulaykhah <zulaykhahogunmola@gmail.com>
2023-02-11 08:28:47 -0500

    changes added

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ let skills = "HTML5,CSS3,GIT,GITHUB,Netlify";
 let fColor = "Burnt Orange";
 let independenceYear = "1960";
 
+// console.log(firstName);
+// console.log(fColor);
+// console.log(skills);
 console.log("I am " + firstName + ",my last name is " + lastName + ". I am a " + track + " Developer. " + "I have skills in " + skills + ".I love the color " + fColor + " so much. " + "Nigeria had independence in " + independenceYear + ".");
