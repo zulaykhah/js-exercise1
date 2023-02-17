44505a8261ef6e5fbdfd2d111f6b785fba076424
Zulaykhah <zulaykhahogunmola@gmail.com>
2023-02-17 12:22:11 -0500

    little update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function multiply (value1, value2) {
-    return value1 * value2;
-}
-console.log(multiply(15,3));
+// QUESTION 1
+let value1 = 15;
+let value2 = 3;
+let multiply = value1 * value2;
+console.log(multiply); 
 
+// QUESTION 2
 let firstName = "Wuraola Suliat";
 let lastName = "Ogunmola";
 let track = "FrontEnd";
@@ -10,7 +12,5 @@ let skills = "HTML5,CSS3,GIT,GITHUB,Netlify";
 let fColor = "Burnt Orange";
 let independenceYear = "1960";
 
-// console.log(firstName);
-// console.log(fColor);
-// console.log(skills);
-console.log("I am " + firstName + ",my last name is " + lastName + ". I am a " + track + " Developer. " + "I have skills in " + skills + ".I love the color " + fColor + " so much. " + "Nigeria had independence in " + independenceYear + ".");
+// PRINTING OUT QUESTION 2
+console.log("I am " + firstName + ",my last name is " + lastName + ". I am a " + track + " Developer. " + "I have skills in " + skills + ".I love the color " + fColor + " so much. " + "Nigeria had independence in October 1st, " + independenceYear + ".");
